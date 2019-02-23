8c0afeb9fc400dc19df127ddedc1d5734af6ee1e
Trọng Thoại <Trng Thoi@DESKTOP-GQHELME>
2019-02-23 21:08:38 +0700

    xây dựng solution

---
+++ WebCoreApp.Infrastructure/SharedKernel/DomainEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebCoreApp.Infrastructure.SharedKernel
+{
+    public abstract class DomainEntity<T>
+    {
+        public T Id { get; set; }
+        public bool IsTransient()
+        {
+            return Id.Equals(default(T));
+        }
+    }
+}
