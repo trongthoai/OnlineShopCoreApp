b9cfedd358285366b7cc1e9b151be5430d539aa2
Trọng Thoại <Trng Thoi@DESKTOP-GQHELME>
2019-02-23 21:22:20 +0700

    build solution

---
+++ WebCoreApp.Data.EF/Configurations/TagConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebCoreApp.Data.EF.Extensions;
+using WebCoreApp.Data.Entities;
+
+namespace WebCoreApp.Data.EF.Configurations
+{
+    public class TagConfiguration : DbEntityConfiguration<Tag>
+    {
+        public override void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Tag> entity)
+        {
+            entity.Property(c => c.Id).HasMaxLength(50)
+                 .IsRequired().HasColumnType("varchar(50");
+        }
+    }
+}
